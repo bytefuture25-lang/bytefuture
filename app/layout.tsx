7d926409f316aa84e060d4c9cfac8946da72ec16
Future Byte <bytefuture25@gmail.com>
2026-06-06 16:57:25 +0530

    Added professional metadata and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-
+import type { Metadata } from "next";
 import { Orbitron, Poppins } from "next/font/google";
 
 const orbitron = Orbitron({
@@ -15,6 +15,48 @@ const poppins = Poppins({
   variable: "--font-poppins",
 });
 
+export const metadata: Metadata = {
+  title: "ByteFuture | Cybersecurity • AI • Development",
+
+  description:
+    "ByteFuture is a technology platform focused on cybersecurity, artificial intelligence, development and digital creativity.",
+
+  keywords: [
+    "Cybersecurity",
+    "Artificial Intelligence",
+    "AI",
+    "Programming",
+    "Development",
+    "Technology",
+    "ByteFuture",
+    "Next.js",
+  ],
+
+  authors: [
+    {
+      name: "Future Byte",
+    },
+  ],
+
+  openGraph: {
+    title: "ByteFuture",
+    description:
+      "Cybersecurity • AI • Development • Content Creation",
+
+    type: "website",
+
+    siteName: "ByteFuture",
+
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "ByteFuture",
+      },
+    ],
+  },
+};
 export default function RootLayout({
   children,
 }: {
@@ -34,3 +76,4 @@ export default function RootLayout({
     </html>
   );
 }
+
